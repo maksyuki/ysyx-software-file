343af165396cb44e1ea05db80adede63e12e882d
maksyuki <maksyuki@126.com>
2021-09-19 20:27:31 +0800

    feat: modify time cycle impl

--- am-kernels/tests/cpu-tests/tests/hello-str.c
+++ am-kernels/tests/cpu-tests/tests/hello-str.c
@@ -4,15 +4,12 @@ char buf[128];
 
 int main() {
 	sprintf(buf, "%s", "Hello world!\n");
-	printf("%s\n", buf);
 	check(strcmp(buf, "Hello world!\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
-	printf("%s\n", buf);
 	check(strcmp(buf, "1 + 1 = 2\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
-	printf("%s\n", buf);
 	check(strcmp(buf, "2 + 10 = 12\n") == 0);
 
 	return 0;
