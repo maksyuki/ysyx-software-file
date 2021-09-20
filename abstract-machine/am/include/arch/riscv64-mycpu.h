97140fe1ee6327addb90666543e0340872b2182d
maksyuki <maksyuki@126.com>
2021-09-20 13:27:17 +0800

    feat: add cte handle code

--- abstract-machine/am/include/arch/riscv64-mycpu.h
+++ abstract-machine/am/include/arch/riscv64-mycpu.h
@@ -3,10 +3,10 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t epc, cause, gpr[32], status;
+  uintptr_t gpr[32], cause, status, epc;
 };
 
-#define GPR1 gpr[0]
+#define GPR1 gpr[17]  // a7
 #define GPR2 gpr[0]
 #define GPR3 gpr[0]
 #define GPR4 gpr[0]
